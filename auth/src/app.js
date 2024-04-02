49e0879acaace0968e85bcd5bf2f4606f4469386
Iqbal Yudiar Syahputra <iqbal.yudiar@dana.id>
2024-04-02 07:34:56 +0700

    feat: add auth src exported and config tailwind

--- auth/src/app.js
+++ auth/src/app.js
@@ -6,4 +6,4 @@ import AuthContent from './components/AuthContent';
 const rootElement = document.getElementById("app");
 if (!rootElement) throw new Error("Failed to find the root element");
 
-export default createApp(AuthContent).use(router).mount(rootElement);;
+export default () => createApp(AuthContent).use(router).mount(rootElement);;
